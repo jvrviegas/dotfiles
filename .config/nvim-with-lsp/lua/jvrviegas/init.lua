2e7f999d4da7b24b695f3152e9c72784b11873c4
joao.viegas <joao.viegas@procimo.com>
2021-10-07 18:27:50 -0300

    feat: add gitsigns and change compe to cmp

--- .config/nvim-with-lsp/lua/jvrviegas/init.lua
+++ .config/nvim-with-lsp/lua/jvrviegas/init.lua
@@ -1,11 +1,12 @@
 require('jvrviegas.telescope')
 require('jvrviegas.blamer')
 require('jvrviegas.lsp')
-require('jvrviegas.autocomplete')
---require('jvrviegas.cmp')
+--require('jvrviegas.autocomplete')
+require('jvrviegas.cmp')
 require('jvrviegas.lualine')
 require('jvrviegas.treesitter')
 --require('jvrviegas.saga')
+require('jvrviegas.gitsigns')
 
 vim.api.nvim_command [[autocmd CursorHold   * lua require'jvrviegas.blamer'.blameVirtText()]]
 vim.api.nvim_command [[autocmd CursorMoved  * lua require'jvrviegas.blamer'.clearBlameVirtText()]]
