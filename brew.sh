b0a3f0dea22ad837f28b39d4b7b99ef2336154ab
joao.viegas <joao.viegas@procimo.com>
2022-01-04 07:34:33 -0300

    chore: update dotfiles

--- brew.sh
+++ brew.sh
@@ -11,44 +11,26 @@ sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 # Install some other useful utilities like `sponge`.
 #brew install moreutils
 
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
 #brew install bash
 brew install wget --with-iri
 brew install bash-completion
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
-brew install grep
+brew install nvim
 brew install openssh
-brew install screen
 brew install python
 
 # Install everything else.
 brew install git
-brew install go
 brew install nmap
 brew install ack
 brew install imagemagick
-brew install tree
 brew install nvm
-brew install ffmpeg
-brew install w3m
-brew install android-sdk
-brew install jpeg
-brew install x264
-brew install cloc
-brew install tldr
-brew install gh
-brew install ctop
 # Rip Grep
 brew install rg
 # Find replace
 brew install fd
-# Cat replace
-brew install bat
-brew install htop
-brew install glances
 
 # Clean it up.
 brew cleanup
