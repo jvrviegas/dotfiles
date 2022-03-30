3857d818311d3b10c8b4384f689a4cfa62e53412
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2022-03-30 14:03:28 -0300

    refactor(brew): comment some packages

--- brew.sh
+++ brew.sh
@@ -6,31 +6,31 @@ brew upgrade
 
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 #brew install moreutils
 
 #brew install bash
-brew install wget --with-iri
-brew install bash-completion
+# brew install wget --with-iri
+# brew install bash-completion
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install nvim
 brew install openssh
-brew install python
+# brew install python
 
 # Install everything else.
 brew install git
 brew install nmap
 brew install ack
-brew install imagemagick
+# brew install imagemagick
 brew install nvm
 # Rip Grep
 brew install rg
 # Find replace
-brew install fd
+brew install fzf
 
 # Clean it up.
 brew cleanup
