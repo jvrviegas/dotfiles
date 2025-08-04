5a6058b84b68a7c934fc470bdcd6d14ae3343f32
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2025-08-04 23:16:59 +0100

    feat: add sketchybar config

---
+++ .config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+	text = "SF Pro", -- Used for text
+	numbers = "SFMono Nerd Font", -- Used for numbers
+
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Regular",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Heavy",
+		["Black"] = "Black",
+	},
+}
