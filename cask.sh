0bed6f7ae8d1fd099ffebd16609d346704369053
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2025-08-26 22:14:20 +0100

    chore: update cask script

--- cask.sh
+++ cask.sh
@@ -8,37 +8,31 @@ function installcask() {
 brew tap homebrew/cask-fonts
 
 # Browsers
-# installcask google-chrome
-# installcask brave-browser
+installcask google-chrome
+installcask zen
+installcask microsoft-edge
 
 # Code
-installcask gpgtools
+installcask ghostty
 installcask docker
 installcask orbstack
 installcask postman
-# installcask visual-studio-code
-installcask insomnia
+installcask visual-studio-code
 installcask dbeaver-community
 installcask mongodb-compass
-installcask kitty
+installcask android-studio
+installcask gcloud-cli
 
 # Fonts
-installcask font-caskaydia-cove-nerd-font
 installcask font-jetbrains-mono-nerd-font
 installcask font-hack-nerd-font
 
 # Others
 installcask raycast
 installcask notion
-# installcask slack
 installcask spotify
-installcask discord
-installcask gimp
-installcask inkdrop
-installcask iterm2
 installcask karabiner-elements
-installcask menumeters
 installcask scroll-reverser
-# installcask telegram
-# installcask textmate
-# installcask zoom
+installcask android-file-transfer
+installcask todoist-app
+installcask akiflow
